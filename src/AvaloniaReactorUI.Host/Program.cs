f10c59125074be1c2ec14930d9498399becd5a6a
Adolfo Marinucci <adospace@msn.com>
2021-10-01 19:07:05 +0200

    Porting to .net 5 + nullables

--- src/AvaloniaReactorUI.Host/Program.cs
+++ src/AvaloniaReactorUI.Host/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Reflection;
@@ -10,51 +11,84 @@ namespace AvaloniaReactorUI.Host
     {
         public static int Main(string[] args)
         {
-#if DEBUG
             if (args == null ||
                 args.Length == 0)
             {
-                args = new[] { @"AvaloniaReactorUI.DemoApp.dll" };
-            }
-#endif
-            if (args == null ||
-                args.Length == 0)
-            {
-                Console.WriteLine("Specify the assembly path to load");
+                Trace.WriteLine("[AvaloniaReactorUI] Specify the assembly path to load");
                 return -1;
             }
 
             var assemblyPath = args[0];
 
+            if (string.IsNullOrWhiteSpace(assemblyPath))
+            {
+                Trace.WriteLine($"[AvaloniaReactorUI] Invalid assembly path: empty string passed");
+                return -1;
+            }
+
+            bool isDll = Path.GetExtension(assemblyPath).ToLowerInvariant() == ".dll";
+            if (isDll)
+            {
+                Trace.WriteLine($"[AvaloniaReactorUI] WARNING: Assembly path extension is not '.dll'");
+            }
+
             if (!Path.IsPathRooted(assemblyPath))
             {
-                assemblyPath = Path.Combine(
-                    Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), assemblyPath);
+                var currentAssemblyFolder = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)
+                    ?? throw new InvalidOperationException("Unable to get current assembly folder path");
+
+                assemblyPath = Path.Combine(currentAssemblyFolder, assemblyPath);
             }
 
             if (!File.Exists(assemblyPath))
             {
-                Console.WriteLine($"The assembly '{assemblyPath}' doesn't exist");
+                Trace.WriteLine($"[AvaloniaReactorUI] The assembly '{assemblyPath}' doesn't exist");
                 return -1;
             }
 
-            var assemblyPdbPath = Path.Combine(Path.GetDirectoryName(assemblyPath), Path.GetFileNameWithoutExtension(assemblyPath) + ".pdb");
+            string folderPath = Path.GetDirectoryName(assemblyPath) ?? throw new InvalidOperationException("Unable to get path of the assembly to hot-reload");
+            string assemblyName = Path.GetFileNameWithoutExtension(assemblyPath);
 
-            var assembly = File.Exists(assemblyPdbPath) ?
-                Assembly.Load(File.ReadAllBytes(assemblyPath)) 
-                :
-                Assembly.Load(File.ReadAllBytes(assemblyPath), File.ReadAllBytes(assemblyPdbPath));
+            Trace.WriteLine($"[AvaloniaReactorUI] Loading assembly '{assemblyPath}'");
+
+            //<assemblyName>.deps.json --> AvaloniaReactorUI.Host.deps.json
+            Trace.WriteLine($"[AvaloniaReactorUI] Copying assembly '{Path.Combine(folderPath, assemblyName + ".deps.json")}' to '{Path.Combine(folderPath, "AvaloniaReactorUI.Host.deps.json")}'");
+            File.Copy(Path.Combine(folderPath, assemblyName + ".deps.json"), Path.Combine(folderPath, "AvaloniaReactorUI.Host.deps.json"), true);
+
+            Assembly? assembly;
+            try
+            {
+                assembly = Utils.LoadAssemblyWithoutLock(assemblyPath);
+            }
+            catch (BadImageFormatException ex)
+            {
+                throw new InvalidOperationException($"Unable to load assembly '{assemblyPath}'{(!isDll ? "Isn't a dll?" : string.Empty)}", ex);
+            }
 
             ComponentLoader.Instance = new AssemblyFileComponentLoader(assemblyPath);
 
-            assembly
-                .GetTypes()
-                .First(_ => _.Name == "Program")
-                .GetMethod("Main")
-                .Invoke(null, new object[] { args.Skip(1).ToArray() });
+            AppDomain currentDomain = AppDomain.CurrentDomain;
 
+            currentDomain.AssemblyResolve += (object? sender, ResolveEventArgs args) =>
+            {
+                string assemblyPath = Path.Combine(folderPath, new AssemblyName(args.Name).Name + ".dll");
+
+                if (!File.Exists(assemblyPath))
+                {
+                    Trace.WriteLine($"[AvaloniaReactorUI] Unable to load '{assemblyPath}'");
+                    return null;
+                }
+
+                var assembly = Utils.LoadAssemblyWithoutLock(assemblyPath);
+                Trace.WriteLine($"[AvaloniaReactorUI] Loaded '{assemblyPath}'");
+                return assembly;
+           };
+
+            var mainMethod = ((assembly ?? throw new InvalidOperationException())
+                .EntryPoint ?? throw new InvalidOperationException())
+                .Invoke(null, new string?[] { null });
+                
             return 0;
         }
-
     }
 }
---
+++ src/AvaloniaReactorUI.HotReloader/Program.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+using static SimpleExec.Command;
+
+namespace AvaloniaReactorUI.HotReloader
+{
+    class Program
+    {
+        private static string? _folderToMonitor;
+        private static FileSystemWatcher? _fileSystemWatcher;
+        private static string? _assemblyPath;
+
+        static void Main(string[] args)
+        {
+            if (args == null || args.Length != 2)
+            {
+                Console.WriteLine("Command line arguments required: <folder-to-monitor> <assembly-dll-path>");
+                return;
+            }
+
+            _folderToMonitor = args[0];
+            _assemblyPath = args[1];
+
+            TryBuildProject(true);
+
+            _fileSystemWatcher = new FileSystemWatcher(
+                _folderToMonitor,
+                "*.cs")
+            {
+                NotifyFilter = NotifyFilters.Attributes |
+                    NotifyFilters.CreationTime |
+                    NotifyFilters.FileName |
+                    NotifyFilters.LastAccess |
+                    NotifyFilters.LastWrite |
+                    NotifyFilters.Size |
+                    NotifyFilters.Security,
+                IncludeSubdirectories = true
+            };
+
+            _fileSystemWatcher.Changed += OnFileChanged;
+            _fileSystemWatcher.Error += OnFileError;
+
+            _fileSystemWatcher.EnableRaisingEvents = true;
+
+            var exitEvent = new AutoResetEvent(false);
+            Console.CancelKeyPress += (s, e) => exitEvent.Set();
+
+            Console.WriteLine($"Waiting for file changes in {_folderToMonitor} ...");
+
+            exitEvent.WaitOne();
+
+
+            _fileSystemWatcher.EnableRaisingEvents = false;
+            _fileSystemWatcher.Changed -= OnFileChanged;
+            _fileSystemWatcher.Error -= OnFileError;
+            _fileSystemWatcher.Dispose();
+            _fileSystemWatcher = null;
+        }
+
+        private static void OnFileError(object sender, ErrorEventArgs e)
+        {
+            Console.WriteLine($"Unable to continue monitoring folder {_folderToMonitor}");
+        }
+
+        private static void OnFileChanged(object sender, FileSystemEventArgs e)
+        {
+            Console.WriteLine($"File changed {e.ChangeType} '{e.FullPath}'");
+
+            if (_fileSystemWatcher == null)
+            {
+                return;
+            }
+
+            _fileSystemWatcher.EnableRaisingEvents = false;
+
+            try
+            {
+                //if (!File.Exists(_assemblyPdbPath))
+                //{
+                var pdbAssemblyName = Path.GetFileNameWithoutExtension(_assemblyPath) + ".pdb";
+                var pdbAssemblyPath = Path.Combine(Path.GetDirectoryName(_assemblyPath) ?? throw new InvalidOperationException(), pdbAssemblyName);
+
+                Console.WriteLine($"Creating empty pdb file {pdbAssemblyPath}...");
+                File.Delete(pdbAssemblyPath);
+                File.WriteAllBytes(pdbAssemblyPath, Array.Empty<byte>());
+                //}
+
+                if (!TryBuildProject())
+                {
+                    TryBuildProject();
+                }
+            }
+            finally
+            {
+                Console.WriteLine("Waiting for file changes...");
+                _fileSystemWatcher.EnableRaisingEvents = true;
+            }
+        }
+
+        private static bool TryBuildProject(bool fullBuild = false)
+        {
+            try
+            {
+                var outputFolder = Path.Combine(_folderToMonitor, $"bin/WpfReactorUI/temp_generated");
+
+                var cmdLine = $"build {(!fullBuild ? "--no-restore --no-dependencies" : "")} --output \"{outputFolder}\"";
+                Console.WriteLine($"Executing '{cmdLine}'");
+                Run("dotnet", cmdLine, _folderToMonitor);
+
+                var pdbAssemblyName = Path.GetFileNameWithoutExtension(_assemblyPath) + ".pdb";
+                var pdbAssemblyPath = Path.Combine(Path.GetDirectoryName(_assemblyPath), pdbAssemblyName);
+                var generatedPdbFilePath = Path.Combine(outputFolder, pdbAssemblyName);
+
+                Console.WriteLine($"Copy from {generatedPdbFilePath} to {pdbAssemblyPath}");
+                File.Copy(generatedPdbFilePath, pdbAssemblyPath, true);
+
+                var assemblyName = Path.GetFileName(_assemblyPath);
+                var generatedFilePath = Path.Combine(outputFolder, assemblyName);
+
+                Console.WriteLine($"Copy from {generatedFilePath} to {_assemblyPath}");
+                File.Copy(generatedFilePath, _assemblyPath, true);
+
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+                //error while running dotnet build...
+                Console.WriteLine(ex);
+                return false;
+            }
+        }
+    }
+}
